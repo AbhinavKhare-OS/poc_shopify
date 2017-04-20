775dd7e3edbd485cc49cbebdda2a854d8ad03c24
Abhinav <abhinav.khare@dreamorbit.com>
2017-04-20 18:53:14 +0530

    reinitialise proh=ject

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "c6c8d02007a48480ecf7190108856512"
+  config.secret = "5fa7f267db1e11824b2e20fb87eb1bc4"
+  config.scope = "read_orders,"
+  config.embedded_app = true
+end
